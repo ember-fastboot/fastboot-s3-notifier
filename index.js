32492f543d61b5089ee5ab1d275d89785f62da03
Jeff Jewiss <jeffjewiss@gmail.com>
2017-10-03 16:04:48 +0100

    add support for custom endpoint

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ class S3Notifier {
     this.s3 = new AWS.S3({
       apiVersion: '2006-03-01',
       signatureVersion: 'v4',
-      region: options.region
+      region: options.region,
+      endpoint: options.endpoint
     });
   }
 
